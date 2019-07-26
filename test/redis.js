24c802300365ee51d6c224ce66f6c0ab24bd7b17
Paulo McNally <paulomcnally@gmail.com>
2019-07-26 00:47:24 -0600

    Change redis test

--- test/redis.js
+++ test/redis.js
@@ -1,17 +1,28 @@
-const chai = require('chai');
+const assert = require('assert');
 const redis = require('redis');
-
-const expect = chai.expect;
-
-const client = redis.createClient({
-  host: '192.168.1.220',
-  port: 6379
-});
+let client;
 
 describe('Redis', function() {
+  before(function () {
+    client = null;
+  });
+
   describe('Connection', function() {
-    it('Valid', function() {
-      expect(client.connected).to.equal(true);
+    it('Valid', function(done) {
+      client = redis.createClient({
+        host: '192.168.1.220',
+        port: 6379,
+        connect_timeout: 1000
+      });
+
+      client.on('connect', function () {
+        assert.strictEqual(client.stream.listeners('timeout').length, 0);
+        client.on('ready', done);
+      });
     });
+  });
+
+  after(function() {
+    client.end(true);
   });
 })
